5aa68ce4267aa0fd533059ef6412b3466b4f0df5
jakevin99 <jakevin9928@gmail.com>
2024-11-04 15:26:58 +0800

    Initial Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to Svelte 5</h1>
-<p class="text-green-400">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Main from "../components/Main.svelte";
+</script>
+
+<Main/>
